cfa9961f3dae407a074d51f41f2165ce86d29297
Apurva Memani <amemani2018@pingry.org>
2016-03-03 11:16:23 -0500

    added comments

--- Characterlimit.py
+++ Characterlimit.py
@@ -1,14 +1,14 @@
 f = open("Characterlimit.txt",'r')
-contents = f.readlines()
+contents = f.readlines()#Gets contents of File
 f.close()
 f = open("output.txt","w")
 
 for item in contents:
-	item = item[0:10]
-	item = item.strip("\n")
+	item = item[0:10]#Counts characters
+	item = item.strip("\n")#Gets rid of extra \n
 	f.write(item + "\n")
 
-
+f.close()
 
 	
 	
