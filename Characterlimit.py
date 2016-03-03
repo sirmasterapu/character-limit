683eb144077d2aebd2591e2f1fa59293a6356e28
Apurva Memani <amemani2018@pingry.org>
2016-03-03 11:10:41 -0500

    Final Version

--- Characterlimit.py
+++ Characterlimit.py
@@ -1,10 +1,11 @@
-f = open("Characterlimit.txt".'r')
+f = open("Characterlimit.txt",'r')
 contents = f.readlines()
 f.close()
 f = open("output.txt","w")
 
 for item in contents:
 	item = item[0:10]
+	item = item.strip("\n")
 	f.write(item + "\n")
 
 
